57e5505d63dd85861dede5c664c19f2fd0dda89a
cerine <saljahdali0205@outlook.com>
2026-02-20 23:41:25 +0300

    Add initial admin terminal layout and style

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
 
 function App() {
+
+  const help = `Available commands:
+- lsProj             : List all projects.
+- delProj            : Delete a project.
+- editProj           : Edit a project.
+- createProj         : Create a new project.
+- lsMsgs             : List all received messages.
+- exit               : Exit the admin terminal.`;
+
   return (
     <div className="App">
            <HashRouter >
             <Routes>
               <Route path ="/" element={<PortfolioLayout />} />
-              <Route path ="/Admin" element={<Admin />} />
+              <Route path ="/Admin" element={<Admin email="admin@example.com" help={help} />} />
                 <Route element={<PortfolioLayout />} path={"*"}></Route>
             </Routes>
             
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,74 @@
 import "./Admin.css";
+import { useState,useRef,useEffect } from "react";
 
-function Admin() {
+function Admin({email, help}) {
+  const [command, setCommand] = useState("");
+  const [output, setOutput] = useState([]);
+
+  const terminalEndRef = useRef(null);
+
+  const handleCommand = (e) => {
+    if (e.key === "Enter") {
+      if (command.trim().toLowerCase() === "login") {
+        setOutput([
+          ...output,
+          { text: `${email}:~$ ${command}`, className: "command" },
+          { text: "Authentication successful! Welcome, Admin.", className: "success" }
+        ]);
+      }else if (command.trim().toLowerCase() === "clear") {
+        setOutput([]);
+      } else if (command.trim().toLowerCase() === "help") {
+        setOutput([
+          ...output,
+          { text: `${email}:~$ ${command}`, className: "command" },
+          { text: help, className: "console-text" }
+        ]);
+      }
+      else{
+        setOutput([
+          ...output,
+          { text: `${email}:~$ ${command}`, className: "command" },
+          { text: "Command not recognized", className: "error" }
+        ]);
+      }
+      setCommand("");
+    }
+  };
+
+  useEffect(() => {
+  terminalEndRef.current?.scrollIntoView({ behavior: "smooth" });
+}, [output]); // تحريك الترمنال عند تغيير output
+
   return (
-    <div className="admin">  
-        <h2>Admin Panel</h2>   
+    <div className="admin">
+      <div className="terminal">
+        <div className="terminal-header">
+          <span className="dot red"></span>
+          <span className="dot yellow"></span>
+          <span className="dot green"></span>
+          <p className="terminal-title">Admin Dashboard</p>
         </div>
+
+        <div className="terminal-content">
+          <p className="terminal-text">Portfolio Admin Terminal v1.0</p>
+          <p className="login-instruction">Type "login" to authenticate.</p>
+
+          <p className="terminal-output">{output.map((line, index) =>
+            <span key={index} className={line.className}>{line.text}<br /></span>)}</p>
+          <input
+            className="terminal-input"
+            value={command}
+            onChange={(e) => setCommand(e.target.value)}
+            onKeyDown={handleCommand}
+            autoFocus
+          />
+
+<div ref={terminalEndRef}></div>
+
+        </div>
+      </div>
+    </div>
   );
-}   
+}
 
 export default Admin;
